b570a30688027528f14c21b3e0a19c09f3747dc6
Tizzzio <s00028424@epicode.edu.mt>
2026-05-15 11:40:23 +0200

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/movie-db/",
+  base: "/Progetto-Front-End-Esame-20-06/",
   plugins: [react()],
   server: { port: 5173 },
 });
